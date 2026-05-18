056e0bf209c27eb701d7ff7bffc92e1a7a8e8e10
Patryk Persak <abogusiac@gmail.com>
2026-05-18 13:59:19 +0200

    Add currency entity

---
+++ src/main/java/com/margomarket/margomarket/model/Currency.java
@@ -0,0 +1,21 @@
+package com.margomarket.margomarket.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "currencies")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Currency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
