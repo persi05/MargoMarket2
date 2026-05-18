b07904c15470f3bde67a71efd4d674623978aef0
Patryk Persak <abogusiac@gmail.com>
2026-05-18 18:07:14 +0200

    Add role repository

---
+++ src/main/java/com/margomarket/margomarket/Repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.margomarket.margomarket.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.margomarket.margomarket.model.Role;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
