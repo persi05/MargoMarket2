bbe7a96185411cdcc342cf1dfd606993e0ce9d7c
Patryk Persak <abogusiac@gmail.com>
2026-05-18 13:59:46 +0200

    Add listing status entity

---
+++ src/main/java/com/margomarket/margomarket/model/ListingStatus.java
@@ -0,0 +1,21 @@
+package com.margomarket.margomarket.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "listing_statuses")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ListingStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
