13b54f15903954fbc7be8fbb09ef0c5d45117916
Patryk Persak <abogusiac@gmail.com>
2026-05-18 13:51:40 +0200

    Add Role entity

---
+++ src/main/java/com/margomarket/margomarket/model/Role.java
@@ -0,0 +1,23 @@
+package com.margomarket.margomarket.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
