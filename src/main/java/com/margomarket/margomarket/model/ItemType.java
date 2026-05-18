77fc462e822b07a822896c2f450806f69fdd2303
Patryk Persak <abogusiac@gmail.com>
2026-05-18 13:56:16 +0200

    Add item type entity

---
+++ src/main/java/com/margomarket/margomarket/model/ItemType.java
@@ -0,0 +1,21 @@
+package com.margomarket.margomarket.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "item_types")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ItemType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+}
