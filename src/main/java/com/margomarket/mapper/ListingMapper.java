157a265cb536172b703b57f335aa4afafc4d9355
Patryk Persak <abogusiac@gmail.com>
2026-05-20 15:48:25 +0200

    Add listing mapper

---
+++ src/main/java/com/margomarket/mapper/ListingMapper.java
@@ -0,0 +1,32 @@
+package com.margomarket.mapper;
+
+import com.margomarket.dto.ListingResponse;
+import com.margomarket.model.Listing;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ListingMapper {
+
+    private final LookupMapper lookupMapper;
+
+    public ListingResponse toResponse(Listing listing) {
+        return new ListingResponse(
+                listing.getId(),
+                listing.getItemName(),
+                lookupMapper.toResponse(listing.getItemType()),
+                listing.getLevel(),
+                lookupMapper.toResponse(listing.getRarity()),
+                listing.getPrice(),
+                lookupMapper.toResponse(listing.getCurrency()),
+                lookupMapper.toResponse(listing.getServer()),
+                listing.getContact(),
+                listing.getStatus().getName(),
+                listing.getUser().getId(),
+                listing.getUser().getEmail(),
+                listing.getCreatedAt(),
+                listing.getSoldAt()
+        );
+    }
+}
